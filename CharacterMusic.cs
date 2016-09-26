41c040310d1cddec5366e93700e96586424391ef
jsysLz <798357885@qq.com>
2016-09-26 19:26:56 +0800

    提交
    代码修改

---
+++ CharacterMusic.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMusic : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		MusicManager.Instance.characterchooseMusic ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
